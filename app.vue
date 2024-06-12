aaf7e50a0adf2b67af321699e339602154931216
bondzioyo <wojciechswiatek.lbn@gmail.com>
2024-06-12 10:28:00 +0200

    install npm and add test

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    {{ counter }}
+      <button type="button" @click="multiply">addd</button>
     <NuxtWelcome />
   </div>
 </template>
+<script setup>
+const counter = ref(0)
+
+const multiply = () => {
+  counter.value++
+}
+
+</script>
