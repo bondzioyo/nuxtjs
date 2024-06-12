36596a35bc1bd6c4e70d8696def6c10ebc14fc40
bondzioyo <wojciechswiatek.lbn@gmail.com>
2024-06-12 17:17:40 +0200

    change twittercard

--- app.vue
+++ app.vue
@@ -15,7 +15,7 @@ useSeoMeta({
   ogTitle: "My Amazing Site",
   description: "This is my amazing site, let me tell you all about it.",
   ogDescription: "This is my amazing site, let me tell you all about it.",
-  ogImage: "https://example.com/image.png",
+  ogImage: "https://bondzioyo.github.io/assets/angular-BgsP3K5R.webp",
   twitterCard: "summary_large_image",
 });
 </script>
